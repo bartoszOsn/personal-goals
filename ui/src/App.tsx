c4975a8351cc635472d0114448a9f9486a82f7d7
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-02-02 21:08:39 +0100

    install tanstack query and tanstack router

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,15 @@
-import { ComponentExample } from "@/components/component-example";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from '@/router.tsx';
+
+const queryClient = new QueryClient();
 
 export function App() {
-return <ComponentExample />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
---
+++ ui/src/router.tsx
@@ -0,0 +1,28 @@
+import { createRootRoute, createRoute, createRouter, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { ComponentExample } from '@/components/component-example.tsx';
+
+const rootRoute = createRootRoute({
+	component: () => (
+		<>
+			<Outlet />
+			<TanStackRouterDevtools />
+		</>
+	),
+})
+
+const indexRoute = createRoute({
+	getParentRoute: () => rootRoute,
+	path: "/",
+	component: ComponentExample,
+})
+
+const routeTree = rootRoute.addChildren([indexRoute])
+
+export const router = createRouter({ routeTree })
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router
+	}
+}
