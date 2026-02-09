5fa830a9a2a202ff6a3105fcee897d6ce834d82d
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-02-09 20:50:59 +0100

    SprintDTO has ID

--- src/dto/time/SprintDTO.ts
+++ src/dto/time/SprintDTO.ts
@@ -1,6 +1,7 @@
 import { QuarterDTO } from './QuarterDTO';
 
 export interface SprintDTO {
+	readonly id: string;
 	readonly year: number;
 	readonly quarter: QuarterDTO;
 	readonly yearlyIndex: number;
