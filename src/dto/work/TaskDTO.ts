81a68ea9019bd3d643e811f0989043c82705efa1
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-02-17 23:45:45 +0100

    Task status data type

--- src/dto/work/TaskDTO.ts
+++ src/dto/work/TaskDTO.ts
@@ -2,8 +2,10 @@ export interface TaskDTO {
 	readonly id: string;
 	readonly name: string;
 	readonly description: string;
-	readonly status: 'TODO' | 'IN_PROGRESS' | 'DONE' | 'FAILED';
+	readonly status: TaskStatusDTO;
 	readonly startDate?: string;
 	readonly endDate?: string;
 	readonly sprintIds: string[];
 }
+
+export type TaskStatusDTO = 'TODO' | 'IN_PROGRESS' | 'DONE' | 'FAILED';
