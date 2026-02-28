f0bd754883f75dbc333f6e3464d58f7e92df0658
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-02-28 18:22:21 +0100

    TaskStatusInplace.tsx option icon

--- ui/src/core/task/inplace/TaskStatusInplace.tsx
+++ ui/src/core/task/inplace/TaskStatusInplace.tsx
@@ -1,12 +1,14 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, ForwardRefExoticComponent } from 'react';
 import { InplaceBadgeDisplay } from '@/base/inplace-editor/api/primitive/display/InplaceBadgeDisplay.tsx';
 import { InplaceSelectEdit } from '@/base/inplace-editor/api/primitive/edit/InplaceSelectEdit.tsx';
 import { useUpdateTaskMutation } from '@/api/task/task-hooks.ts';
 import { InplaceEditorDisplay } from '@/base/inplace-editor/api/InplaceEditorDisplay.tsx';
 import { InplaceEditorEdit } from '@/base/inplace-editor/api/InplaceEditorEdit.tsx';
 import { InplaceEditor } from '@/base/inplace-editor/api/InplaceEditor.tsx';
-import { ComboboxData, MantineColor } from '@mantine/core';
+import { Text, ComboboxItem, ComboboxLikeRenderOptionInput, Group, MantineColor } from '@mantine/core';
 import { Task, TaskStatus } from '@/models/Task';
+import { IconCheck, IconProgress, IconProgressCheck, IconProgressHelp, IconProgressX, IconProps } from '@tabler/icons-react';
+import * as react from 'react';
 
 export interface TaskStatusInplaceProps {
 	task: Task;
@@ -26,7 +28,21 @@ export function TaskStatusInplace({ task, badgeProps, selectProps}: TaskStatusIn
 		});
 	}
 
-	const options: ComboboxData = Object.entries(statusToName).map(([value, label]) => ({ value, label }));
+	const options: Array<ComboboxItem> = Object.entries(statusToName).map(([value, label]) => ({ value, label }));
+
+	const renderOption = (option: ComboboxLikeRenderOptionInput<ComboboxItem>) => {
+		const Icon = statusToIcon[option.option.value as TaskStatus];
+		const color = statusToColor[option.option.value as TaskStatus];
+		const cssColor = `var(--mantine-color-${color}-5)`;
+
+		return (
+			<Group gap='xs' w='100%'>
+				<Icon size={16} color={cssColor} />
+				<Text>{option.option.label}</Text>
+				{ option.checked && <IconCheck style={{ marginInlineStart: 'auto' }} {...checkIconProps} />}
+			</Group>
+		);
+	}
 
 	return (
 		<InplaceEditor loading={taskMutation.isPending}>
@@ -36,6 +52,7 @@ export function TaskStatusInplace({ task, badgeProps, selectProps}: TaskStatusIn
 			<InplaceEditorEdit>
 				<InplaceSelectEdit defaultValue={task.status}
 								   data={options}
+								   renderOption={renderOption}
 								   onValueSubmit={(value) => value && onValueSubmit(value as TaskStatus)}
 								   {...selectProps} />
 			</InplaceEditorEdit>
@@ -55,4 +72,18 @@ const statusToColor: Record<TaskStatus, MantineColor> = {
 	[TaskStatus.IN_PROGRESS]: 'blue',
 	[TaskStatus.DONE]: 'green',
 	[TaskStatus.FAILED]: 'red'
+};
+
+const statusToIcon: Record<TaskStatus, ForwardRefExoticComponent<IconProps & react.RefAttributes<SVGSVGElement>>> = {
+	[TaskStatus.TODO]: IconProgressHelp,
+	[TaskStatus.IN_PROGRESS]: IconProgress,
+	[TaskStatus.DONE]: IconProgressCheck,
+	[TaskStatus.FAILED]: IconProgressX
+}
+
+const checkIconProps = {
+	stroke: 1.5,
+	color: 'currentColor',
+	opacity: 0.6,
+	size: 18,
 };
