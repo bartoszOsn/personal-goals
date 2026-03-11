3fbbe33da72fd2a208b59453bb60d3c97caf85e9
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-03-11 21:12:11 +0100

    Sprint overview creation

--- src/dto/work-item/WorkItemCreationRequestDTO.ts
+++ src/dto/work-item/WorkItemCreationRequestDTO.ts
@@ -1,7 +1,9 @@
 import { WorkItemTypeDTO } from './WorkItemTypeDTO.js';
+import { WorkItemUpdateRequestDTO } from '../../../types/index.js';
 
 export interface WorkItemCreationRequestDTO {
 	context: number;
 	type: WorkItemTypeDTO;
 	parentId?: string;
+	fields?: WorkItemUpdateRequestDTO;
 }
