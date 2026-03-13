7f55fa211c1dd4689808bfb2c7ba37477bb24d39
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-03-13 22:23:11 +0100

    sprint presentation layer

---
+++ api/src/presentation/sprint/SprintController.ts
@@ -0,0 +1,82 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseArrayPipe,
+	ParseIntPipe,
+	Post,
+	Put
+} from '@nestjs/common';
+import { SprintDTO, SprintsUpdateRequestDTO } from '@personal-okr/shared';
+import { ContextYear } from '../../domain/common/model/ContextYear';
+import { SprintId } from '../../domain/sprint/model/SprintId';
+import { SprintService } from '../../app/sprint/SprintService';
+import { SprintDTOConverter } from './SprintDTOConverter';
+
+@Controller('sprint')
+export class SprintController {
+	constructor(
+		private readonly sprintService: SprintService,
+		private readonly sprintDTOConverter: SprintDTOConverter
+	) {}
+
+	@Get(':context')
+	async getSprintsByContext(
+		@Param('context', new ParseIntPipe()) contextRaw: number
+	): Promise<SprintDTO[]> {
+		const context = new ContextYear(contextRaw);
+		const sprintCollection =
+			await this.sprintService.getSprintsByContext(context);
+
+		return this.sprintDTOConverter.toSprintsDTO(sprintCollection);
+	}
+
+	@Post(':context')
+	async createSprint(
+		@Param('context', new ParseIntPipe()) contextRaw: number
+	): Promise<SprintDTO> {
+		const context = new ContextYear(contextRaw);
+		const sprintCollection = await this.sprintService.createSprint(context);
+		return this.sprintDTOConverter.toSprintsDTO(sprintCollection);
+	}
+
+	@Post(':context/fill')
+	async fillSprints(
+		@Param('context', new ParseIntPipe()) contextRaw: number
+	): Promise<SprintDTO[]> {
+		const context = new ContextYear(contextRaw);
+		const sprintCollection = await this.sprintService.fillSprints(context);
+		return this.sprintDTOConverter.toSprintsDTO(sprintCollection);
+	}
+
+	@Put(':context')
+	async updateSprints(
+		@Param('context', new ParseIntPipe()) contextRaw: number,
+		@Body() body: SprintsUpdateRequestDTO
+	): Promise<SprintDTO[]> {
+		const context = new ContextYear(contextRaw);
+		const requests =
+			this.sprintDTOConverter.fromSprintsUpdateRequestDTO(body);
+		const sprintCollection = await this.sprintService.updateSprints(
+			context,
+			requests
+		);
+		return this.sprintDTOConverter.toSprintsDTO(sprintCollection);
+	}
+
+	@Delete(':context/:ids')
+	async deleteSprints(
+		@Param('context', new ParseIntPipe()) contextRaw: number,
+		@Param('ids', new ParseArrayPipe({ separator: ',' })) idsRaw: string[]
+	): Promise<SprintDTO[]> {
+		const context = new ContextYear(contextRaw);
+		const ids = idsRaw.map((id) => new SprintId(id));
+		const sprintCollection = await this.sprintService.deleteSprints(
+			context,
+			ids
+		);
+		return this.sprintDTOConverter.toSprintsDTO(sprintCollection);
+	}
+}
---
+++ api/src/presentation/sprint/SprintDTOConverter.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { SprintContextCollection } from '../../domain/sprint/model/SprintContextCollection';
+import {
+	SprintDTO,
+	SprintsUpdateRequestDTO,
+	SprintUpdateRequestDTO
+} from '@personal-okr/shared';
+import { quarterToNumber } from '../../domain/common/model/Quarter';
+import { SprintUpdateRequest } from '../../domain/sprint/model/SprintUpdateRequest';
+import { SprintId } from '../../domain/sprint/model/SprintId';
+import { Temporal } from 'temporal-polyfill';
+
+@Injectable()
+export class SprintDTOConverter {
+	toSprintsDTO(sprintCollection: SprintContextCollection): SprintDTO[] {
+		return sprintCollection.sprints.map((sprint) => ({
+			id: sprint.id.value,
+			name: sprint.name,
+			quarter: quarterToNumber(sprint.quarter),
+			startDate: sprint.startDate.toString(),
+			endDate: sprint.endDate.toString()
+		}));
+	}
+
+	fromSprintsUpdateRequestDTO(
+		dto: SprintsUpdateRequestDTO
+	): SprintUpdateRequest[] {
+		return Object.entries(dto).map(
+			([id, sprint]: [string, SprintUpdateRequestDTO]) =>
+				new SprintUpdateRequest(
+					new SprintId(id),
+					sprint.startDate === undefined
+						? undefined
+						: Temporal.PlainDate.from(sprint.startDate),
+					sprint.endDate === undefined
+						? undefined
+						: Temporal.PlainDate.from(sprint.endDate)
+				)
+		);
+	}
+}
