ecac7f6b0786d3cf9060fb259db5e4aeb2254c20
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-04-09 21:47:21 +0200

    board dnd auto scroll

--- ui/src/base/board/internal/BoardMain.tsx
+++ ui/src/base/board/internal/BoardMain.tsx
@@ -1,13 +1,32 @@
 import { useAtomValue } from 'jotai';
+import { autoScrollForElements, autoScrollWindowForElements } from '@atlaskit/pragmatic-drag-and-drop-auto-scroll/element';
 import { getBoardAtoms } from '@/base/board/internal/state/getBoardAtoms';
 import { Group, LoadingOverlay, ScrollArea } from '@mantine/core';
 import { BoardColumn } from '@/base/board/internal/BoardColumn.tsx';
+import { useEffect, useRef } from 'react';
+import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
 
 export function BoardMain<TData, TColumnId>() {
 	const props = useAtomValue(getBoardAtoms<TData, TColumnId>().propsAtom);
 	const showLoadingOverlay = useAtomValue(getBoardAtoms<TData, TColumnId>().showLoadingOverlayAtom);
+
+	const scrollAreaRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!scrollAreaRef.current) {
+			return;
+		}
+
+		return combine(
+			autoScrollForElements({
+				element: scrollAreaRef.current,
+			}),
+			autoScrollWindowForElements()
+		);
+	}, []);
+
 	return (
-		<ScrollArea.Autosize scrollbars="x" offsetScrollbars>
+		<ScrollArea.Autosize viewportRef={scrollAreaRef} scrollbars="x" offsetScrollbars>
 			<LoadingOverlay visible={showLoadingOverlay} />
 			<Group wrap="nowrap" align="stretch" pos="relative">
 				{
