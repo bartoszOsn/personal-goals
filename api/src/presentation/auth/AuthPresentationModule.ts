140bc2d3e1466b2e657c83290245dc0cd6bd815a
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-04-19 01:01:30 +0200

    Remove unused packages

--- api/src/presentation/auth/AuthPresentationModule.ts
+++ api/src/presentation/auth/AuthPresentationModule.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './guards/JwtAuthGuard';
 import { AuthAppModule } from '../../app/auth/AuthAppModule';
 import { AuthInfrastructureModule } from '../../infrastructure/auth/AuthInfrastructureModule';
 
 @Module({
-	imports: [
-		PassportModule,
-		AuthAppModule.withRepositories(AuthInfrastructureModule)
-	],
+	imports: [AuthAppModule.withRepositories(AuthInfrastructureModule)],
 	providers: [
 		{
 			provide: APP_GUARD,
