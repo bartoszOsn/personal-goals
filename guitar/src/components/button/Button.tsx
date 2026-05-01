378dc9263f04d5e043afe467dce352e6a21c78b8
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-05-01 12:19:47 +0200

    Button sizes

--- guitar/src/components/button/Button.tsx
+++ guitar/src/components/button/Button.tsx
@@ -7,9 +7,9 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	size?: 'compact' | 'default' | 'large';
 }
 
-export function Button({ variant = 'neutral', ...baseProps }: ButtonProps) {
+export function Button({ variant = 'neutral', size = 'default', ...baseProps }: ButtonProps) {
 	return (
-		<button className={`gui-button ${variantToClass[variant]}`} {...baseProps}>
+		<button className={`gui-button ${variantToClass[variant]} ${sizeToClass[size]}`} {...baseProps}>
 			Button
 		</button>
 	);
@@ -18,4 +18,10 @@ export function Button({ variant = 'neutral', ...baseProps }: ButtonProps) {
 const variantToClass: Record<NonNullable<ButtonProps['variant']>, string> = {
 	primary: 'gui-button--primary',
 	neutral: 'gui-button--neutral',
+}
+
+const sizeToClass: Record<NonNullable<ButtonProps['size']>, string> = {
+	compact: 'gui-button--compact',
+	default: 'gui-button--default',
+	large: 'gui-button--large',
 }
