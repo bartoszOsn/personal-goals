d6d8fcfc2fbfe15614baea70965151a49361622e
Bartosz Osiński <osinskibartosz24@gmail.com>
2026-05-25 23:58:57 +0200

    Add `@firebase-oss/ui-core` and `@firebase-oss/ui-translations` dependencies, update `yarn.lock`, and adjust deployment script

--- cn-deploy-ui.sh
+++ cn-deploy-ui.sh
@@ -1,3 +1,5 @@
 alias yarn="./yarn/yarn";
 corepack yarn install;
+corepack yarn workspaces foreach -Rip --from @personal-okr/shared run build;
+corepack yarn install;
 corepack yarn workspaces foreach -Rip --from @personal-okr/ui run build;
